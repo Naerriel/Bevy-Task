7c69cb2c80887e9527ec31c6533c842c92135ae9
Naerriel <naerriel@gmail.com>
2018-06-03 15:13:05 +0200

    Improvement of "Skillset" display in "Overview"

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -6,8 +6,79 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './index.styl'
 
+import upperBody from './assets/upperBody.png'
+import lowerBody from './assets/lowerBody.png'
+import endurance from './assets/endurance.png'
+import agility from './assets/agility.png'
+import aesthetics from './assets/aesthetics.png'
+
+const maxLevel = 5;
+
 export default class Overview extends React.Component {
     // Can change spans with className="label" to just labels
+    resetExtremeSkills() {
+        Object.keys(this.props.skillset).map((skill) => {
+            let thisSkill = document.querySelector(`#${skill}`);
+            thisSkill.classList.remove("bestSkill");
+            thisSkill.classList.remove("worstSkill");
+        });
+    }
+
+    selectExtremeSkills(props) {
+        this.resetExtremeSkills();
+        let bestSkills = [];
+        let bestSkillLevel = 0;
+        let worstSkills = [];
+        let worstSkillLevel = maxLevel;
+
+        Object.keys(props.skillset).map((skill) => {
+            const level = props.skillset[skill];
+            if(level > bestSkillLevel) {
+                bestSkillLevel = level;
+                bestSkills = [];
+            }
+            if(level === bestSkillLevel) {
+                bestSkills.push(skill);
+            }
+            if(level < worstSkillLevel) {
+                worstSkillLevel = level;
+                worstSkills = [];
+            }
+            if(level === worstSkillLevel) {
+                worstSkills.push(skill);
+            }
+        });
+        bestSkills.forEach((skill) => {
+            document.querySelector(`#${skill}`).classList.add("bestSkill");
+        });
+        worstSkills.forEach((skill) => {
+            document.querySelector(`#${skill}`).classList.add("worstSkill");
+        });
+    }
+
+    componentDidMount() {
+        this.selectExtremeSkills(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.selectExtremeSkills(nextProps);
+    }
+
+    renderSkillImage(skill){
+        switch(skill){
+            case 'upperBody':
+                    return <img src={upperBody} />
+            case 'lowerBody':
+                    return <img src={lowerBody} />
+            case 'endurance':
+                    return <img src={endurance} />
+            case 'agility':
+                    return <img src={agility} />
+            case 'aesthetics':
+                    return <img src={aesthetics} />
+        }
+    }
+
     render() {
         return (
             <section className="l-section c-overview" >
@@ -19,9 +90,11 @@ export default class Overview extends React.Component {
                     <div className="skillset">
                         {Object.keys(this.props.skillset).map((skill) => {
                             return (
-                                <div key={skill}>
-                                    <span className="list-item">{`${skill}: `}</span>
-                                    {this.props.skillset[skill]}
+                                <div className="skill" key={skill}>
+                                    {this.renderSkillImage(skill)}
+                                    <figcaption id={skill}>
+                                        {this.props.skillset[skill]}
+                                    </figcaption>
                                 </div>
                             );
                         })}
