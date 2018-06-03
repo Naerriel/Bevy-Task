891ebcf0fb2d0e80a56f1641170f3601903d8edc
Naerriel <naerriel@gmail.com>
2018-06-03 16:34:37 +0200

    Add filtering and sorting of predictions

--- src/components/Predictions/index.js
+++ src/components/Predictions/index.js
@@ -8,7 +8,30 @@ import { disciplineScore, skillScore } from '../../libs/calculate'
 import './index.styl'
 
 export default class Predictions extends React.Component {
-    render() {
+    constructor() {
+        super();
+
+        this.state = {
+            sort: "none",
+            filter: "none"
+        };
+        this.sortChange = this.sortChange.bind(this);
+        this.filterChange = this.filterChange.bind(this);
+    }
+
+    sortChange(e) {
+        this.setState({ sort: e.target.value });
+    }
+
+    filterChange(e) {
+        this.setState({ filter: e.target.value });
+    }
+
+    componentWillMount() {
+        this.createPredictions();
+    }
+
+    createPredictions() {
         let predictionsTable = [...this.props.disciplines];
         predictionsTable.map((discipline) => {
             discipline.score = disciplineScore(this.props.athlete.skillset, discipline.requirements);
@@ -18,23 +41,68 @@ export default class Predictions extends React.Component {
                 discipline.drilldown[skill] = score;
             });
         });
+        this.setState({ predictions: predictionsTable });
+    }
+
+    disciplineHTML(discipline) {
+        return (
+            <div key={discipline.name} className="c-discipline">
+                <span className="name">
+                  {discipline.name}
+                </span>
+                <span className="score">
+                  {disciplineScore(this.props.athlete.skillset, discipline.requirements)}
+                </span>
+            </div>
+        );
+    }
+
+    switchFilter(filter) {
+        switch(filter){
+            case 'none':
+                return this.state.predictions.map((discipline) => this.disciplineHTML(discipline));
+            case 'individual':
+                return this.state.predictions.filter(function(discipline) {
+                    return discipline.isIndividual;
+                }).map((discipline) => this.disciplineHTML(discipline));
+            case 'team':
+                return this.state.predictions.filter(function(discipline) {
+                    return !discipline.isIndividual;
+                }).map((discipline) => this.disciplineHTML(discipline));
+        }
+    }
+
+    render() {
+
+        if(this.state.sort === "ascend") {
+            this.state.predictions.sort(function(first, second) {
+                return first.score - second.score;
+            });
+        }
+
+        if(this.state.sort === "descend") {
+            this.state.predictions.sort(function(first, second) {
+                return second.score - first.score;
+            });
+        }
+
         return (
             <section className="l-section c-predictions" >
                 <h2 className="header" >Predictions</h2>
+                <label>Sort by:</label>
+                <select onChange={this.sortChange} value={this.state.sort}>
+                    <option value="none">Don't sort</option>
+                    <option value="ascend">Score ascending</option>
+                    <option value="descend">Score descending</option>
+                </select>
+                <label>Filter:</label>
+                <select onChange={this.filterChange} value={this.state.filter}>
+                    <option value="none">None</option>
+                    <option value="individual">Individual sport</option>
+                    <option value="team">Team sport</option>
+                </select>
                 <div className="content">
-                    {this.props.disciplines.map((discipline) => {
-                        return (
-                            <div key={discipline.name} className="c-discipline">
-                                <span className="name">
-                                  {discipline.name}
-                                </span>
-                                -
-                                <span className="score">
-                                  {disciplineScore(this.props.athlete.skillset, discipline.requirements)}
-                                </span>
-                            </div>
-                        );
-                    })}
+                    {this.switchFilter(this.state.filter)}
                 </div>
             </section>
         );
